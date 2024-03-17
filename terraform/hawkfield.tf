58b7566c110219201e6a76baff38720bc5444a96
angus306 <ANGUS306@bailliegifford.com>
2024-03-17 17:45:38 +0000

    don't cluster

--- terraform/hawkfield.tf
+++ terraform/hawkfield.tf
@@ -2,16 +2,16 @@ data vsphere_datacenter "hl-01" {
   name = "hl-dc-01"
 }
 
-resource "vsphere_compute_cluster" "hl-cluster" {
-  datacenter_id = data.vsphere_datacenter.hl-01.id
-  name          = "hl-cluster-01"
-}
+#resource "vsphere_compute_cluster" "hl-cluster" {
+#  datacenter_id = data.vsphere_datacenter.hl-01.id
+#  name          = "hl-cluster-01"
+#}
 
 resource "vsphere_host" "esxi01" {
   hostname = "10.1.2.11"
   username = var.vsphere_user
   password = var.vsphere_password
-  cluster  = vsphere_compute_cluster.hl-cluster.id
+#  cluster  = vsphere_compute_cluster.hl-cluster.id
   license  = var.licence_key
 }
 
@@ -19,7 +19,7 @@ resource "vsphere_host" "esxi02" {
   hostname = "10.1.2.12"
   username = var.vsphere_user
   password = var.vsphere_password
-  cluster  = vsphere_compute_cluster.hl-cluster.id
+#  cluster  = vsphere_compute_cluster.hl-cluster.id
   license  = var.licence_key
 }
 
@@ -27,7 +27,7 @@ resource "vsphere_host" "esxi03" {
   hostname = "10.1.2.13"
   username = var.vsphere_user
   password = var.vsphere_password
-  cluster  = vsphere_compute_cluster.hl-cluster.id
+#  cluster  = vsphere_compute_cluster.hl-cluster.id
   license  = var.licence_key
 }
 
